94f438f630a5cbbea77ebbc1f4e752b0341c342d
avenmia <mavend2@gmail.com>
2020-11-15 13:13:49 -0600

    Add code for listing containers

--- ResumeCache/index.ts
+++ ResumeCache/index.ts
@@ -1,12 +1,27 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+import { BlobServiceClient, StorageSharedKeyCredential } from "@azure/storage-blob";
+
+import * as dotenv from "dotenv";
+dotenv.config();
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest): Promise<void> {
-    context.log('HTTP trigger function processed a request.');
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+   
+    const account = process.env.ACCOUNT_NAME || "";
+    const accountKey = process.env.ACCOUNT_KEY || "";
+    const sharedKeyCredential = new StorageSharedKeyCredential(account, accountKey);
 
+    const blobServiceClient = new BlobServiceClient(
+        // When using AnonymousCredential, following url should include a valid SAS or support public access
+        `https://${account}.blob.core.windows.net`,
+        sharedKeyCredential
+      );
+    
+      let i = 1;
+      for await (const container of blobServiceClient.listContainers()) {
+        console.log(`Container ${i++}: ${container.name}`);
+      }
+    
+    const responseMessage = "Hello";
     context.res = {
         // status: 200, /* Defaults to 200 */
         body: responseMessage
